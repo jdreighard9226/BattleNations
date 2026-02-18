6cf059dd51109db20de7e5fb0be92f3badc44420
JD Reighard <jreighard9226@gmail.com>
2026-02-18 13:21:49 -0700

    set up src folder, a test package, and test code

---
+++ src/test/Test.java
@@ -0,0 +1,11 @@
+package test;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("<---Welcome To BattleNations--->");
+        System.out.println("Max, you should play RagDoll Archers when you are bored in General classes.");
+        System.out.println("Game Link: https://www.crazygames.com/game/ragdoll-archers");
+    }
+}
