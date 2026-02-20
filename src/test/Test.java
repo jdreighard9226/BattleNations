f72dc5d8d10594240732656be33324c03c926bef
Max Montowski <mmontowski5@carroll.edu>
2026-02-20 09:28:54 -0700

    Committing in my test branch to share it with main branch.

--- src/test/Test.java
+++ src/test/Test.java
@@ -7,5 +7,6 @@ public class Test {
         System.out.println("<---Welcome To BattleNations--->");
         System.out.println("Max, you should play RagDoll Archers when you are bored in General classes.");
         System.out.println("Game Link: https://www.crazygames.com/game/ragdoll-archers");
+        System.out.println("I tired it out, that was a good recomendation.");
     }
 }
