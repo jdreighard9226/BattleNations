dda4c7d42bb8236041f81b04ed21aaa1f4d1623e
JD Reighard <jreighard9226@gmail.com>
2026-02-26 15:29:00 -0700

    built out initial player class

---
+++ src/player/Player.java
@@ -0,0 +1,61 @@
+/**
+ * Description: This file represents a Player for Battle Nations.
+ * The Player has a name, a color, and a number of troops they are allowed to place.
+ * The number of troops to place will be calculated and passed to player each round.
+ *
+ * Filename: Player.Java
+ * Author: J.D. Reighard
+ * Date: 2/26/2026
+ */
+package player;
+import java.awt.Color;
+
+public class Player {
+    // class level variables
+    private String name;
+    private Color color;
+    private int troopsToPlace;
+
+    /**
+     * Constructor for player
+     * @param name the name this player will go by
+     * @param color the color this player's territories will be.
+     */
+    public Player(String name, Color color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    /**
+     * get amount of troops the player can place
+     * @return troupsToPlace
+     */
+    public int getTroopsToPlace() {
+        return troopsToPlace;
+    }
+
+    /**
+     * get the name of player
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * get the color of player
+     * @return color
+     */
+    public Color getColor() {
+        return color;
+    }
+
+    /**
+     * set the amount of troops a player can place
+     * @param troopsToPlace number of troops
+     */
+    public void setTroopsToPlace(int troopsToPlace) {
+        this.troopsToPlace = troopsToPlace;
+    }
+}
+
