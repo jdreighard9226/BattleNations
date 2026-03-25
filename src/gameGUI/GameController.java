8cb7be82c6170298c82417e268713a36c10c2aa8
JD Reighard <jreighard9226@gmail.com>
2026-03-25 01:34:54 -0600

    updated gameController to build out a backend gameLogic instance to have acess to backend methods

--- src/gameGUI/GameController.java
+++ src/gameGUI/GameController.java
@@ -1,14 +1,19 @@
 package gameGUI;
 
-import map.AbstractWorld;
+import game.AttackService;
+import game.FortifyService;
+import game.GameLogic;
+import game.ReinforcementService;
+import map.TotalDominationWorld;
 import player.Player;
 
 import javax.swing.*;
 import java.util.List;
 
 public class GameController {
-
-    public GameController(AbstractWorld world, List<Player> players, JFrame display){
+    private GameLogic gameLogic;
+    public GameController(TotalDominationWorld world, List<Player> players, JFrame display){
+        gameLogic = new GameLogic(world, players, new AttackService(), new ReinforcementService(), new FortifyService());
         System.exit(0);
     }
 }
