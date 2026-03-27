e1f15c14e8c9e298a2c0daec5f23143cfa6f995b
JD Reighard <jreighard9226@gmail.com>
2026-03-26 21:05:46 -0600

    pushing local changes to this branch before grabbing newest master

--- src/gameGUI/GameController.java
+++ src/gameGUI/GameController.java
@@ -14,13 +14,17 @@ import java.awt.*;
 import java.util.List;
 
 public class GameController {
-    private GameLogic gameLogic;
+    private final GameLogic gameLogic;
     private List<Territory> territories;
+
+    private Territory firstTerritoryClicked;
+    private  Territory secondTerritoryClicked;
     public GameController(TotalDominationWorld world, List<Player> players, JFrame display, MapPanel mapPanel){
         gameLogic = new GameLogic(world, players, new AttackService(), new ReinforcementService(), new FortifyService());
         territories = world.getAllTerritories();
         mapPanel.switchToGameController(this);
-        System.exit(0);
+        display.revalidate();
+        display.repaint();
     }
 
     public void getTerritoryClicked(Point point) {
