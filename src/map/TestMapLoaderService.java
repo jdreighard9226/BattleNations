02bb0f962559f87b989336a27ca71447e303de58
Max Montowski <mmontowski5@carroll.edu>
2026-04-17 01:52:53 -0600

    Added files for testing

--- src/map/TestMapLoaderService.java
+++ src/map/TestMapLoaderService.java
@@ -1,4 +1,11 @@
 package map;
 
 public class TestMapLoaderService {
+
+    public static void main(String[] args) {
+        boolean passed = true;
+
+        System.out.println("Testing MapLoaderService");
+        if (!testRead)
+    }
 }
